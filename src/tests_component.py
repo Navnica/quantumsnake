eedd33a281726bbc47af0dabf6587fe6a6778951
Navnca <navnica@outlook.com>
2024-04-27 16:56:50 +0300

    asf

--- src/tests_component.py
+++ src/tests_component.py
@@ -7,7 +7,6 @@ class TestsComponent(flet.SafeArea):
 
     def build(self):
         self.content = flet.Column(
-            scroll=flet.ScrollMode.ADAPTIVE,
             horizontal_alignment=flet.CrossAxisAlignment.CENTER,
             controls=[
                 flet.Stack(
@@ -33,62 +32,23 @@ class TestsComponent(flet.SafeArea):
                         )
                     ]
                 ),
+                flet.Divider(height=10),
                 flet.GridView(
                     runs_count=2,
-                    spacing=5,
-                    expand=True,
+                    max_extent=170,
                     controls=[
-                        flet.ExpansionTile(
-                            title=flet.Text("Эмоции"),
-                            leading=flet.Icon(name=flet.icons.EMOJI_EMOTIONS),
-                            collapsed_text_color=flet.colors.WHITE,
-                            text_color=flet.colors.PRIMARY,
+                        flet.Container(
                             bgcolor=flet.colors.SURFACE_VARIANT,
-                            shape=flet.RoundedRectangleBorder(radius=10),
-                            collapsed_shape=flet.RoundedRectangleBorder(radius=10),
-                            controls=[
-                                flet.FilledButton(text="Пройти")
-                            ],
-                        ),
-
-                        flet.ExpansionTile(
-                            title=flet.Text("Ответы"),
-                            leading=flet.Icon(name=flet.icons.QUESTION_ANSWER),
-                            collapsed_text_color=flet.colors.WHITE,
-                            text_color=flet.colors.PRIMARY,
-                            bgcolor=flet.colors.SURFACE_VARIANT,
-                            shape=flet.RoundedRectangleBorder(radius=10),
-                            collapsed_shape=flet.RoundedRectangleBorder(radius=10),
-                            controls=[
-                                flet.FilledButton(text="Пройти")
-                            ],
-                        ),
-
-                        flet.ExpansionTile(
-                            title=flet.Text("Время"),
-                            leading=flet.Icon(name=flet.icons.ACCESS_TIME_FILLED),
-                            collapsed_text_color=flet.colors.WHITE,
-                            text_color=flet.colors.PRIMARY,
-                            bgcolor=flet.colors.SURFACE_VARIANT,
-                            shape=flet.RoundedRectangleBorder(radius=10),
-                            collapsed_shape=flet.RoundedRectangleBorder(radius=10),
-                            controls=[
-                                flet.FilledButton(text="Пройти")
-                            ],
-                        ),
-
-                        flet.ExpansionTile(
-                            title=flet.Text("Наречия"),
-                            leading=flet.Icon(name=flet.icons.QUESTION_ANSWER),
-                            collapsed_text_color=flet.colors.WHITE,
-                            text_color=flet.colors.PRIMARY,
-                            bgcolor=flet.colors.SURFACE_VARIANT,
-                            shape=flet.RoundedRectangleBorder(radius=10),
-                            collapsed_shape=flet.RoundedRectangleBorder(radius=10),
-                            controls=[
-                                flet.FilledButton(text="Пройти")
-                            ],
-                        ),
+                            border_radius=15,
+                            content=flet.Column(
+                                horizontal_alignment=flet.CrossAxisAlignment.CENTER,
+                                alignment=flet.MainAxisAlignment.CENTER,
+                                controls=[
+                                    flet.Icon(name=flet.icons.EMOJI_EMOTIONS, size=55),
+                                    flet.FilledButton(text='Эмоции')
+                                ]
+                            )
+                        )
                     ]
                 )
             ]
