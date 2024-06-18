1e7086f7ef8e53ae5cf52e455021939f13860dad
Navnica <navnica@outlook.com>
2024-06-19 02:52:45 +0300

    ревью сам не знаю чего

--- src/client/auth_component.py
+++ src/client/auth_component.py
@@ -11,28 +11,25 @@ class AuthComponent(flet.SafeArea):
 
     def build(self) -> None:
         def set_session(username: str) -> None:
-            response_user: requests.Response = requests.get(
+            response_user = requests.get(
                 url=f'{SETTINGS.SERVER_URL}/user/get_by_username/{username}',
                 params={'token': SETTINGS.TOKEN}
             )
 
-            response_role: requests.Response = requests.get(
+            response_role = requests.get(
                 url=f'{SETTINGS.SERVER_URL}/role/get/{response_user.json()["role_id"]}',
                 params={'token': SETTINGS.TOKEN}
             )
 
-            files: list = []
-
-            for file in requests.get(
+            files = [
+                Session.VideoFiles.VideoFile(name=file['name'], tag=file['tag'])
+                for file in requests.get(
                     url=f'{SETTINGS.SERVER_URL}/video_file/get_all',
                     params={'token': SETTINGS.TOKEN}
-            ).json():
-                files.append(Session.VideoFiles.VideoFile(
-                    name=file['name'],
-                    tag=file['tag']
-                ))
+                ).json()
+            ]
 
-            session: Session = Session(
+            session = Session(
                 user=response_user.json(),
                 role=response_role.json(),
                 video_files=files
@@ -43,22 +40,21 @@ class AuthComponent(flet.SafeArea):
             self.opacity = 0
 
         def set_connection_info(fail: bool) -> None:
-            color: str = flet.colors.GREEN if not fail else flet.colors.RED
-            text: str = 'Успешно' if not fail else 'Не удалось'
+            color = flet.colors.GREEN if not fail else flet.colors.RED
+            text = 'Успешно' if not fail else 'Не удалось'
 
             self.content.controls[2].color = color
             self.content.controls[2].value = text
             self.content.controls[0].controls[-1].content = flet.Icon(
                 name=flet.icons.DONE if not fail else flet.icons.PRIORITY_HIGH, color=color)
             self.content.controls[0].controls[0].value = 1
-            self.content.controls[4].visible = True if fail and SETTINGS.DEBUG else False
+            self.content.controls[4].visible = fail and SETTINGS.DEBUG
 
             self.content.controls[0].controls[0].color = color
 
         async def check_connection():
             await asyncio.sleep(3)
-
-            response_status_code: int = 0
+            response_status_code = 0
 
             try:
                 response_status_code = requests.get(SETTINGS.SERVER_URL).status_code
@@ -68,7 +64,6 @@ class AuthComponent(flet.SafeArea):
             if response_status_code == 200:
                 set_connection_info(fail=False)
                 self.page.loop.create_task(start_animation())
-
             else:
                 set_connection_info(fail=True)
 
@@ -89,18 +84,17 @@ class AuthComponent(flet.SafeArea):
             self.update()
 
         async def on_login_click(event: flet.ControlEvent) -> None:
-            login_text_field: flet.TextField = auth_content.controls[0].content.controls[0]
-            password_text_field: flet.TextField = auth_content.controls[0].content.controls[1]
+            login_text_field = auth_content.controls[0].content.controls[0]
+            password_text_field = auth_content.controls[0].content.controls[1]
 
-            if login_text_field.value == '' or password_text_field.value == '':
-                if login_text_field.value == '':
+            if not login_text_field.value or not password_text_field.value:
+                if not login_text_field.value:
                     login_text_field.error_text = 'Поле пусто'
 
-                if password_text_field.value == '':
+                if not password_text_field.value:
                     password_text_field.error_text = 'Поле пусто'
-
             else:
-                response: requests.Response = requests.post(
+                response = requests.post(
                     url=SETTINGS.SERVER_URL + '/user_auth/login',
                     json={
                         'username': login_text_field.value,
@@ -109,16 +103,10 @@ class AuthComponent(flet.SafeArea):
                     params={'token': SETTINGS.TOKEN}
                 )
 
-                match response.status_code:
-                    case 200:
-                        if response.json():
-                            set_session(login_text_field.value)
-
-                        else:
-                            login_text_field.error_text = 'Неверное имя пользователя или пароль'
-
-                    case 401:
-                        pass
+                if response.status_code == 200 and response.json():
+                    set_session(login_text_field.value)
+                else:
+                    login_text_field.error_text = 'Неверное имя пользователя или пароль'
 
             self.page.update()
 
@@ -129,30 +117,28 @@ class AuthComponent(flet.SafeArea):
                 self.update()
                 return
 
-            login_text_field: flet.TextField = auth_content.controls[0].content.controls[0]
-            password_text_field: flet.TextField = auth_content.controls[0].content.controls[1]
-            confirm_password_text_field: flet.TextField = auth_content.controls[0].content.controls[2]
+            login_text_field = auth_content.controls[0].content.controls[0]
+            password_text_field = auth_content.controls[0].content.controls[1]
+            confirm_password_text_field = auth_content.controls[0].content.controls[2]
 
             if (
-                    login_text_field.value == '' or
-                    password_text_field.value == '' or
-                    confirm_password_text_field.value == '' or
-                    password_text_field.value != confirm_password_text_field.value
+                not login_text_field.value or
+                not password_text_field.value or
+                not confirm_password_text_field.value or
+                password_text_field.value != confirm_password_text_field.value
             ):
-                if login_text_field.value == '':
+                if not login_text_field.value:
                     login_text_field.error_text = 'Поле пусто'
 
-                if password_text_field.value == '':
+                if not password_text_field.value:
                     password_text_field.error_text = 'Поле пусто'
 
-                if confirm_password_text_field.value == '':
+                if not confirm_password_text_field.value:
                     confirm_password_text_field.error_text = 'Поле пусто'
-
                 elif password_text_field.value != confirm_password_text_field.value:
                     confirm_password_text_field.error_text = 'Пароли не совпадают'
-
             else:
-                response: requests.Response = requests.post(
+                response = requests.post(
                     url=SETTINGS.SERVER_URL + '/user_auth/register',
                     json={
                         'username': login_text_field.value,
@@ -161,12 +147,10 @@ class AuthComponent(flet.SafeArea):
                     params={'token': SETTINGS.TOKEN}
                 )
 
-                match response.status_code:
-                    case 200:
-                        set_session(login_text_field.value)
-
-                    case 409:
-                        login_text_field.error_text = 'Имя пользователя занято'
+                if response.status_code == 200:
+                    set_session(login_text_field.value)
+                elif response.status_code == 409:
+                    login_text_field.error_text = 'Имя пользователя занято'
 
             self.page.update()
 
@@ -177,10 +161,9 @@ class AuthComponent(flet.SafeArea):
 
                 self.page.dialog.open = False
                 self.page.update()
-
                 self.page.loop.create_task(check_connection())
 
-            server_address_text_field: flet.TextField = flet.TextField(
+            server_address_text_field = flet.TextField(
                 label='Адрес сервера',
                 value=SETTINGS.SERVER_URL,
                 border_radius=10,
@@ -188,7 +171,7 @@ class AuthComponent(flet.SafeArea):
                 border_color=flet.colors.PRIMARY,
             )
 
-            token_text_field: flet.TextField = flet.TextField(
+            token_text_field = flet.TextField(
                 label='Токен',
                 value=SETTINGS.TOKEN,
                 border_radius=10,
@@ -206,12 +189,7 @@ class AuthComponent(flet.SafeArea):
                     controls=[
                         server_address_text_field,
                         token_text_field,
-
-                        flet.FilledButton(
-                            text='Сохранить',
-                            on_click=save_settings
-                        )
-
+                        flet.FilledButton(text='Сохранить', on_click=save_settings)
                     ]
                 )
             )
@@ -220,7 +198,7 @@ class AuthComponent(flet.SafeArea):
         self.animate_opacity = flet.Animation(duration=200)
         self.on_animation_end = end_animation
 
-        start_content: flet.Column = flet.Column(
+        start_content = flet.Column(
             alignment=flet.MainAxisAlignment.CENTER,
             horizontal_alignment=flet.CrossAxisAlignment.CENTER,
             controls=[
@@ -229,18 +207,13 @@ class AuthComponent(flet.SafeArea):
                     height=70,
                     alignment=flet.alignment.center,
                     controls=[
-                        flet.ProgressRing(
-                            width=70,
-                            height=70
-                        ),
-
+                        flet.ProgressRing(width=70, height=70),
                         flet.Container(
                             alignment=flet.alignment.center,
                             content=flet.Icon(name=flet.icons.MORE_HORIZ)
                         )
                     ]
                 ),
-
                 flet.Divider(height=20, color=flet.colors.with_opacity(0, flet.colors.GREEN)),
                 flet.Text(value='Подключение к серверу'),
                 flet.Divider(height=20, color=flet.colors.with_opacity(0, flet.colors.GREEN)),
@@ -249,11 +222,10 @@ class AuthComponent(flet.SafeArea):
                     visible=False,
                     on_click=on_connection_setting_click
                 )
-
             ]
         )
 
-        auth_content: flet.Column = flet.Column(
+        auth_content = flet.Column(
             alignment=flet.MainAxisAlignment.END,
             horizontal_alignment=flet.CrossAxisAlignment.CENTER,
             controls=[
@@ -270,7 +242,6 @@ class AuthComponent(flet.SafeArea):
                                 border_width=2,
                                 border_color=flet.colors.PRIMARY,
                             ),
-
                             flet.TextField(
                                 label='Пароль',
                                 password=True,
@@ -279,7 +250,6 @@ class AuthComponent(flet.SafeArea):
                                 border_width=2,
                                 border_color=flet.colors.PRIMARY
                             ),
-
                             flet.TextField(
                                 label='Повторение пароля',
                                 password=True,
@@ -290,14 +260,12 @@ class AuthComponent(flet.SafeArea):
                                 animate_opacity=flet.Animation(duration=200),
                                 visible=False
                             ),
-
                             flet.Divider(height=20, color=flet.colors.PRIMARY),
                             flet.FilledButton(
                                 text='Войти',
                                 on_click=on_login_click,
                                 width=150
                             ),
-
                             flet.TextButton(
                                 text='Регистрация',
                                 on_click=on_registration_click
@@ -305,7 +273,6 @@ class AuthComponent(flet.SafeArea):
                         ]
                     )
                 )
-
             ]
         )
 
